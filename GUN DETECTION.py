55839bffd06dd708c89a3b95f59c3f1b69de3ac1
kalaivanan1999 <kalaivananpm29@gmail.com>
2025-01-29 20:05:21 +0530

    Add files via upload

---
+++ GUN DETECTION.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2
+import imutils
+import datetime
+
+gun_cascade=cv2.CascadeClassifier(r"C:\Users\Kalai\Downloads\cascade.xml")
+camera=cv2.VideoCapture(0)
+
+
+firstframe=None
+gun_exist=None
+
+while True:
+    
+    ret,frame=camera.read()
+
+    frame=imutils.resize(frame,width=500)
+
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    gun=gun_cascade.detectMultiScale(gray,
+                                     1.3,5,
+                                     minSize=(100,100))
+    
+    if len(gun) >0:
+        gun_exist=True
+
+    for (x,y,w,h) in gun:
+        frame=cv2.rectangle(frame,
+                            (x,y),
+                            (x+w,y+h),
+                            (255,0,0),2) 
+
+        roi_gray=gray[y:y+h,x:x+w]
+        roi_color=frame[y:y+h,x:x+w]
+
+    if firstframe is None:
+        firstframe=gray
+        continue
+
+    cv2.imshow("security feed",frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+
+if gun_exist:
+    print("guns detected")
+else:
+    print("guns didn't detected")
+
+
+camera.release()
+cv2.destroyAllWindows()   
